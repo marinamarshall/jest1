550d21e67dc8572413e673855ca3f0b09019355a
marinamarshall <marinamarshall2@gmail.com>
2022-01-10 07:14:41 +0000

    Add testing of DOM - h1 test broken, need to fix later

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,13 @@
  const buttonClick = require("../button");
 
  beforeEach(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+    //  document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+
  });
  
  describe("DOM tests", () => {
@@ -14,4 +20,11 @@
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
      });
+     // Testing if h1 tag exists
+     test("h1 should exist", () => {
+         // Stored in an array, if h1 exists, length of array will be one
+         expect(document.getElementsByName("h1").length).toBe(1);
+     });
  });
+
+
